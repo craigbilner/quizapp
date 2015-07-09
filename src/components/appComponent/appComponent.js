ba4494ab1965cc74047573e182a4aa84d2f6f22b
Craig Bilner <craigbilner@Craigs-MacBook-Pro.local>
2015-07-09 22:23:38 +0100

    Initial integration of UI prototype

--- src/components/appComponent/appComponent.js
+++ src/components/appComponent/appComponent.js
@@ -1,33 +1,18 @@
 'use strict';
 
 import React from 'react';
+import GameComponent from '../gameComponent/gameComponent.js';
 
 export default class AppComponent extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      text: this.props.text
-    };
-  }
-
-  handleClick() {
-    this.setState({
-      text: 'click attached on the client'
-    });
   }
 
   render() {
     return (
-      <div onClick={this.handleClick.bind(this)}>{this.state.text}</div>
+      <div>
+        <GameComponent />
+      </div>
     );
   }
 }
-
-AppComponent.propTypes = {
-  text: React.PropTypes.string
-};
-
-AppComponent.defaultProps = {
-  text: 'default text'
-};
---
+++ src/components/questionComponent/questionComponent.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+
+export default class QuestionComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Question goes here</div>
+    );
+  }
+}
+
+QuestionComponent.propTypes = {};
+
+QuestionComponent.defaultProps = {};
